8a2618b7fd03c9f986a4ba1144e0a6feec582a30
jacklinke <jacklinke@gmail.com>
2024-09-22 20:51:39 -0600

    Implement users app in settings

--- unveil/unveil/settings.py
+++ unveil/unveil/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.users",
     "apps.core",
 ]
 
@@ -124,6 +125,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "users.UserAccount"
 
 
 # Internationalization

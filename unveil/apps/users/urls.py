a93d759a7d025d74fb60d93ecfd5799c60ff9b37
jacklinke <jacklinke@gmail.com>
2024-09-25 17:16:44 -0400

    Make sure urls include api

--- unveil/apps/users/urls.py
+++ unveil/apps/users/urls.py
@@ -1,15 +1,16 @@
 """URLs for the unveil users app."""
 
-from django.urls import path
-from ninja import NinjaAPI
 from django.contrib.auth.models import User
+from django.urls import path
+from ninja import Router
 
 urlpatterns = []
 
-api = NinjaAPI()
+router = Router()
 
 
-@api.get("/create_account")
+@router.get("/create_account")
 def create_account(request, given_username: str, given_password: str, given_email: str):
-    user = User.objects.create_user(username=given_username, password=given_password, email=given_email)
+    """Create a new user account."""
+    User.objects.create_user(username=given_username, password=given_password, email=given_email)
     return {"success": True}

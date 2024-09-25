f8fd0629c7ee0cf957b2a8c80268a6493421e79a
jacklinke <jacklinke@gmail.com>
2024-09-25 16:36:06 -0400

    Fix user

--- unveil/apps/users/models.py
+++ unveil/apps/users/models.py
@@ -1,22 +1,52 @@
 """Models for the unveil users app."""
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class UserAccount(AbstractUser):
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create a new superuser profile"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
     """Model for custom user accounts."""
 
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, default="None")
 
-    email = models.EmailField(
-        verbose_name="email address",
-        max_length=255,
-        unique=True,
-    )
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
 
     def __str__(self):
+        """Return string representation of our user"""
         return self.email
